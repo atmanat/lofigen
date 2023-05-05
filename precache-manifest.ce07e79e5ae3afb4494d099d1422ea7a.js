51f4e062df05644a1c985ab1db68ba9000a38423
atatat <erdi_0501@hotmail.com>
2023-05-05 21:31:46 +0300

    visual effect removed and button added for noise sound test

---
+++ precache-manifest.ce07e79e5ae3afb4494d099d1422ea7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ce78c0a8f05e04f36d8",
+    "url": "/lofigen/static/css/main.582a16d1.chunk.css"
+  },
+  {
+    "revision": "0ce78c0a8f05e04f36d8",
+    "url": "/lofigen/static/js/main.ac6f2054.chunk.js"
+  },
+  {
+    "revision": "efd189cc024e85981231",
+    "url": "/lofigen/static/js/runtime~main.5a661e9b.js"
+  },
+  {
+    "revision": "b10896808a45be0d9ee0",
+    "url": "/lofigen/static/js/2.72fa8a1a.chunk.js"
+  },
+  {
+    "revision": "152595c53e2e3e5519b2fc98031f2407",
+    "url": "/lofigen/static/media/foo.152595c5.txt"
+  },
+  {
+    "revision": "9e399a9e768bef9903d30a29c9dfd56f",
+    "url": "/lofigen/index.html"
+  }
+];
